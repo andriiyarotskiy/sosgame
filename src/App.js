59911bdfb9c967ca4fa2b8deaf7bbf11116f46ff
Andriy <refwol131@mail.ru>
2021-01-05 10:25:32 +0200

    accordion component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
+import Accordion from "./components/Accordion";
 
 const App =() =>{
   return (
-    <div>
-      Happy Hacking!
-    </div>
+    <Accordion />
   );
 }
 
---
+++ src/components/Data.js
@@ -0,0 +1,5 @@
+export const DataAccordion = [
+    {title: 'title1', subtitle: 'subtitle1'},
+    {title: 'title2', subtitle: 'subtitle2'},
+    {title: 'title3', subtitle: 'subtitle3'},
+]
